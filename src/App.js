1dab8938b3c1c62506cbc20e21b9d1862d2e87af
omaran bazna <omran4d@gmail.com>
2022-08-24 23:39:19 -0400

    add Booklist to the main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Search from "./components/Search";
+import BookList from "./components/BookList";
 function App() {
   return (
     <div className="App">
       <Search />
+      <BookList />
     </div>
   );
 }
