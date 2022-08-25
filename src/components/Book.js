c0a01cca633b4efdfe5d2e78f538aafd429de1df
omaran bazna <omran4d@gmail.com>
2022-08-24 23:38:41 -0400

    add Book component content

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Book = ({ title, author, imgURL, details }) => {
-  return <div></div>;
+  return (
+    <div>
+      <img src={imgURL} />
+      <div>
+        <h1>{title}</h1>
+        <h3>{author}</h3>
+        <p>{author}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Book;
