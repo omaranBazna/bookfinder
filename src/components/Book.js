253aa512f6e141659c01935e4fbbc3fd91a5fac8
omaran bazna <omran4d@gmail.com>
2022-08-24 23:43:29 -0400

    add classes to the book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,13 +2,16 @@ import React from "react";
 
 const Book = ({ title, author, imgURL, details }) => {
   return (
-    <div>
-      <img src={imgURL} />
+    <div className="book">
       <div>
-        <h1>{title}</h1>
-        <h3>{author}</h3>
-        <p>{details}</p>
+        <img className="book-cover" src={imgURL} />
+        <div className="book-details">
+          <h1>{title}</h1>
+          <h3>{author}</h3>
+          <p>{details}</p>
+        </div>
       </div>
+      <button className="book-btn">Details</button>
     </div>
   );
 };
