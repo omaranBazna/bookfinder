90d21c43437536de44ce3b553886e747ffb4a03c
omaran bazna <omran4d@gmail.com>
2022-08-24 23:56:34 -0400

    handle the search input state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { useState } from "react";
 const Search = () => {
+  const [search, setSearch] = useState("");
   return (
     <div>
       <form>
-        <input type="text" placeholder="Search for Book"></input>
+        <input
+          type="text"
+          placeholder="Search for Book"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        ></input>
         <button type="submit">Search</button>
       </form>
     </div>
