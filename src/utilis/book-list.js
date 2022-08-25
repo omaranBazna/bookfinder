3954fa11e0a47e317f558862c72f712fdddeb405
omaran bazna <omran4d@gmail.com>
2022-08-24 23:32:58 -0400

    add book list

---
+++ src/utilis/book-list.js
@@ -0,0 +1,64 @@
+export const books = [
+  {
+    id: "0",
+    title: "Learn to code",
+    author: "Omaran Bazna",
+    Description: "the most helpful book to prepare for coding interview",
+    imgURL:
+      "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+  },
+
+  {
+    id: "1",
+    title: "Learn to code",
+    author: "Omaran Bazna",
+    Description: "the most helpful book to prepare for coding interview",
+    imgURL:
+      "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+  },
+
+  {
+    id: "2",
+    title: "Learn to code",
+    author: "Omaran Bazna",
+    Description: "the most helpful book to prepare for coding interview",
+    imgURL:
+      "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+  },
+
+  {
+    id: "3",
+    title: "Learn to code",
+    author: "Omaran Bazna",
+    Description: "the most helpful book to prepare for coding interview",
+    imgURL:
+      "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+  },
+
+  {
+    id: "4",
+    title: "Learn to code",
+    author: "Omaran Bazna",
+    Description: "the most helpful book to prepare for coding interview",
+    imgURL:
+      "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+  },
+
+  {
+    id: "5",
+    title: "Learn to code",
+    author: "Omaran Bazna",
+    Description: "the most helpful book to prepare for coding interview",
+    imgURL:
+      "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+  },
+
+  {
+    id: "6",
+    title: "Learn to code",
+    author: "Omaran Bazna",
+    Description: "the most helpful book to prepare for coding interview",
+    imgURL:
+      "https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg",
+  },
+];
