5b76cbc94f43e476d025835d6cd021e1dd179784
omaran bazna <omran4d@gmail.com>
2022-08-24 23:36:00 -0400

    map books array to list of Books components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { books } from "../utilis/book-list";
+import Book from "./Book";
 const BookList = ({ searched }) => {
-  return <div></div>;
+  return (
+    <div>
+      {books.map((book) => {
+        <Book key={book.id} />;
+      })}
+    </div>
+  );
 };
 
 export default BookList;
