81268d3f894803bd7ae6193333be0d16d6f3c89c
SssngM <rarang222@gmail.com>
2022-09-01 13:01:01 -0700

    call api

--- src/components/booklist/booklist.js
+++ src/components/booklist/booklist.js
@@ -1,20 +1,57 @@
 import React from 'react'
 import data from '../../booklist.json'
+import { useEffect, useState } from "react";
+
+
 
 
 export const Booklist = (props) => {
-  return (
-    <div>
-        {data.map((item)=>(
-            < li key ={item.id}>  
-                {item.title} 
-                {item.author}
-                {item.image}
-            </li>
-        ))}
-    </div>
-  )
+    const [loading, setLoading] = useState(false)
+    const [items, setItems] = useState([])
+    useEffect (()=>{
+        fetch("https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor")
+        .then((res)=> res.json())
+        .then(
+            (result)=> {
+            setItems(result)
+            setLoading(true)
+        });
+    },[]);
+
+    if (!loading) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+            <div>
+            {items.map((item)=>(
+                < li key ={item.id}>  
+                    {item.title} 
+                    {item.authors}
+                    {item.publishedDate}
+                    {item.image}
+                </li>
+            ))}
+        </div>
+        )
+    }
 }
+
+
+// function Booklist(props) {
+//   return (
+//     <div>
+//         {data.map((item)=>(
+//             < li key ={item.id}>  
+//                 {item.title} 
+//                 {item.author}
+//                 {item.image}
+//             </li>
+//         ))}
+//     </div>
+//   )
+// }
+
+
 
 
 export default Booklist
